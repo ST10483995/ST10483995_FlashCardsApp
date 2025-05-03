d0fa182ba25db64197af216d1ca9ecd11c497b43
ST10483995 <st10483995@vcconnect.edu.za>
2025-05-03 12:09:29 +0200

    First Commit - Score page added - Next Button partially coded - First Array created - Question page set up - Start Button coded

---
+++ app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/FlashCards.kt
@@ -0,0 +1,48 @@
+package za.co.varsitycollege.st10483995.st10483995_flashcardsapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FlashCards : AppCompatActivity() {
+
+    val questions = arrayOf(
+        "The skin is the largest human organ",
+        "O negative is the rarest blood type",
+        "Water boils at 50 degrees Celsius",
+        "Portuguese is the official language of Mozambique",
+        "India is a European country"
+
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_flash_cards)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val questionView = findViewById<TextView>(R.id.questionView)
+        val titleView = findViewById<TextView>(R.id.titleView)
+        val trueButton = findViewById<Button>(R.id.trueButton)
+        val falseButton = findViewById<Button>(R.id.falseButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+        questionView.text = questions[0]
+
+        nextButton.setOnClickListener {
+            var counter = 0
+            while (counter <= 4) {
+                if (counter == 4) {
+                    val intent
+                }
+            }
+
+        }
+    }
+}
--- app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/Score.kt
+++ app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/Score.kt
@@ -1,28 +1,20 @@
 package za.co.varsitycollege.st10483995.st10483995_flashcardsapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Score : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_score)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val startButton = findViewById<Button>(R.id.startButton)
-
-        startButton.setOnClickListener {
-            val intent = Intent(this, FlashCards::class.java)
-            startActivity(intent)
-        }
-        }
     }
+}

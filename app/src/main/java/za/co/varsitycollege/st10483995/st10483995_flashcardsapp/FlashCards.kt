a8448ee03856b212a465851a718a37c1a9a7326e
ST10483995 <st10483995@vcconnect.edu.za>
2025-05-19 21:18:37 +0200

    Fourth Commit

--- app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/FlashCards.kt
+++ app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/FlashCards.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,24 +18,18 @@ class FlashCards : AppCompatActivity() {
         "Water boils at 50 degrees Celsius",
         "Portuguese is the official language of Mozambique",
         "India is a European country"
-
     )
 
-    val answers = arrayOf(
-        "True",
-        "False",
-        "False",
-        "True",
-        "False"
-    )
+    val answers = arrayOf(true, false, false, true, false)
 
-    val useranswer = arrayOf(
-        "",
-        "",
-        "",
-        "",
-        ""
-    )
+    val useranswer = BooleanArray(5)
+
+    var counter = 0
+    var trueFalse = false
+    var falseTrue = false
+    var score = 0
+    var scoreCounter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -50,20 +45,67 @@ class FlashCards : AppCompatActivity() {
         val trueButton = findViewById<Button>(R.id.trueButton)
         val falseButton = findViewById<Button>(R.id.falseButton)
         val nextButton = findViewById<Button>(R.id.nextButton)
-        var counter = 0
-        questionView.text = questions[0]
-       nextButton.setOnClickListener{
-           counter ++
-           questionView.text = questions[counter]
-           trueButton.isEnabled = true
-           falseButton.isEnabled = true
-           if (counter == 4) {
-               val intent = Intent(this, Score::class.java)
-               startActivity(intent)
-           }
 
 
-       }
+        questionView.text = questions[counter]
 
+
+
+        trueButton.setOnClickListener {
+            trueFalse = true
+            falseTrue = false
+        }
+
+        falseButton.setOnClickListener {
+            falseTrue = true
+            trueFalse = true
+        }
+
+
+//        fun check(useranswer: Boolean) {
+//            val correctAnswer: Boolean = answers[counter]
+//            if (useranswer[counter] == correctAnswer) {
+//                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+
+        useranswer[counter] = trueFalse
+
+
+
+        nextButton.setOnClickListener {
+            counter++
+            questionView.text = questions[counter]
+            if (counter == 4) {
+                val intent = Intent(this, Score::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+        if (useranswer[counter] == answers[counter]) {
+            score++
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show()
+        }
+
+
+        if (counter == questions.size - 1) {
+
+            val intent = Intent(this, Score::class.java).apply {
+                putExtra("questions", questions)
+                putExtra("answers", answers)
+                putExtra("useranswer", useranswer)
+                putExtra("score", score)
+
+            }
         }
     }
+
+    private operator fun Boolean.get(counter: Int): Any {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/Score.kt
+++ app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/Score.kt
@@ -1,6 +1,7 @@
 package za.co.varsitycollege.st10483995.st10483995_flashcardsapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,18 @@ class Score : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val scoreView = findViewById<TextView>(R.id.scoreView)
+        val score = intent.getIntExtra("score", 0)
+        val total = intent.getIntExtra("total", 5)
+
+        scoreView.text = "You scored $score/$total"
+                if (score >= 3) {
+                    "Great Job!"
+                } else {
+                    if (score < 3) {
+                        "Keep Practicing"
+                    }
+                }
+
     }
 }

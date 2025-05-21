8380c2cfbe0f74641ecb3340a83b28ef4c0a7b66
ST10483995 <st10483995@vcconnect.edu.za>
2025-05-21 15:51:40 +0200

    Ninth Commit

--- app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/FlashCards.kt
+++ app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/FlashCards.kt
@@ -1,7 +1,10 @@
 package za.co.varsitycollege.st10483995.st10483995_flashcardsapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.icu.lang.UProperty.NameChoice
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -10,28 +13,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+data class QuestionsLists(
+    val question: String,
+    var answer: Boolean,
+    var useranswer: Boolean? = null
+)
+
+var currentIndex = 0
+var scoreCounter = 0
 class FlashCards : AppCompatActivity() {
 
     //Declaration of the arrays
-    val questions = arrayOf(
-        "Queen Elizabeth owned 2000 pairs of gloves",
-        "Ancient Egyptians invented toothpaste 300 years ago",
-        "Ketchup was originally sold as detergent",
-        "Cleopatra married two of her brothers",
-        "Switzerland has the world's oldest parliament"
+    private val questions = mutableListOf(
+        QuestionsLists("Queen Elizabeth owned 2000 pairs of gloves", true),
+        QuestionsLists("Ancient Egyptians invented toothpaste 300 years ago",false),
+        QuestionsLists("Ketchup was originally sold as detergent", false),
+        QuestionsLists("Cleopatra married two of her brothers",true),
+        QuestionsLists("Switzerland has the world's oldest parliament", false)
     )
 
-    val answers = arrayOf(true, false, false, true, false)
-
-    val useranswer = BooleanArray(5)
-
-    //Declarations for the variables
-    var counter = 0
-    var trueFalse = false
-    var falseTrue = false
-    var scoreCounter = 0
 
 
+
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -43,74 +49,97 @@ class FlashCards : AppCompatActivity() {
         }
         // Declarations of ID's
         val questionView = findViewById<TextView>(R.id.questionView)
-        val titleView = findViewById<TextView>(R.id.titleView)
         val trueButton = findViewById<Button>(R.id.trueButton)
         val falseButton = findViewById<Button>(R.id.falseButton)
         val nextButton = findViewById<Button>(R.id.nextButton)
+        val doneButton = findViewById<Button>(R.id.doneButton)
+        doneButton.visibility = View.GONE
 
 
         //To show the questions of the screen
-        questionView.text = questions[counter]
+        questionView.text = questions[currentIndex].question
 
         //Initialising the true button
         trueButton.setOnClickListener {
-            trueFalse = true
-            falseTrue = false
+            falseButton.isEnabled = false
+
+            questions[currentIndex].answer = true
+            checkAnswer(true)
         }
 
         //Initialising the false button
         falseButton.setOnClickListener {
-            falseTrue = true
-            trueFalse = true
+            trueButton.isEnabled = false
+
+            questions[currentIndex].answer = false
+            checkAnswer(false)
+        }
+
+        doneButton.setOnClickListener{
+            val intent = Intent(this, Score::class.java)
+            startActivity(intent)
         }
 
 
         //Corresponding the user answer to a value
-        useranswer[counter] = trueFalse
+        questions[currentIndex].answer = true
 
 
         //Initialising the next button
         nextButton.setOnClickListener {
-            counter++
+            currentIndex++
 
             //Set the questions to change in the TextView
-            questionView.text = questions[counter]
+            questionView.text = questions[currentIndex].question
+
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
 
             //Argument to loop through the questions
-            if (counter == 4) {
-                val intent = Intent(this, Score::class.java)
-                startActivity(intent)
-        }
-
-        //Argument to check across the arrays
-        if (useranswer[counter] == answers[counter]) {
-            scoreCounter++
-
-            //Making sure a text is shown for whether the answer is correct or not
-            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show()
-        }
-        }
-
-        //Assigning the counter to a value
-        val score = scoreCounter
-
-
-
-        if (counter == questions.size - 1) {
-            val intent = Intent(this, Score::class.java).apply {
-                putExtra("questions", questions)
-                putExtra("answers", answers)
-                putExtra("useranswer", useranswer)
-                putExtra("score", score)
-
-                //Starting the intent
-                startActivity(intent)
+            if (currentIndex == 4) {
+                nextButton.visibility = View.GONE
+                doneButton.visibility = View.VISIBLE
             }
         }
 
+
+
+//        if (counter == questions.size - 1) {
+//            val intent = Intent(this, Score::class.java).apply {
+//                putExtra("questions", questions)
+//                putExtra("answer", answer)
+//                putExtra("useranswer", useranswer)
+//                putExtra("score", score)
+//                putExtra()
+//
+//                Starting the intent
+//                startActivity(intent)
+//                finish()
+//            }
+//        }
+
+
+    }
+    private fun checkAnswer(userChoice: Boolean){
+        val questionNow = questions[currentIndex]
+        questionNow.useranswer = userChoice
+
+        val correct = questionNow.useranswer == questionNow.answer
+
+        Toast.makeText(
+            this,
+            if (correct) "Correct!"
+            else "Incorrect",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        if (correct){
+            scoreCounter++
         }
     }
+
+
+}
+
 
 
--- app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/Score.kt
+++ app/src/main/java/za/co/varsitycollege/st10483995/st10483995_flashcardsapp/Score.kt
@@ -1,5 +1,6 @@
 package za.co.varsitycollege.st10483995.st10483995_flashcardsapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -22,28 +23,34 @@ class Score : AppCompatActivity() {
         //Declarations
         val reviewButton = findViewById<Button>(R.id.reviewButton)
         val reviewView = findViewById<TextView>(R.id.reviewView)
-        val total = intent.getIntExtra("total", 5)
+        val closeButton = findViewById<Button>(R.id.closeButton)
+        val feedbackText = findViewById<TextView>(R.id.feedbackText)
+        val total = 5
+        val score = scoreCounter
 
-
-//        val questions = intent.getSerializableExtra("questions") as ArrayList<String>
-//        val answers = intent.getSerializableExtra("answers") as ArrayList<String>
-//        val useranswer = intent.getSerializableExtra("useranswer") as ArrayList<String>
-        val score = intent.getIntExtra("score", 0)
 
         //Declaring an setting a text view
         val scoreView = findViewById<TextView>(R.id.scoreView)
         scoreView.text = "You scored $score/$total"
-//        val scoreBuilder = StringBuilder()
-//        scoreView.text = scoreBuilder.toString()
+
 
         //Setting the button to show the question reviews
         reviewButton.setOnClickListener{
-//            reviewDisplay = "Question 1 = True\n"
-//            "Question 2 = False\n"
-//            "Question 3 = False\n"
-//            "Question 4 = True\n"
-//            "Question 5 = False"
+            reviewView.text = "Question 1 = True\n" + "Question 2 = False\n" + "Question 3 = False\n" + "Question 4 = True\n" + "Question 5 = False"
+
+
         }
+
+        closeButton.setOnClickListener {
+            finishAffinity()
+        }
+
+        val feedback = when {
+            score >= 3 -> "Great Job!"
+            else -> "Keep Practicing"
+        }
+
+        feedbackText.text = feedback
     }
 }
 
